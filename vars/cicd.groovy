80331b10bb61bc92c173d19686aaec2da1a473bf
bheemamahi <126442653+bheemamahi@users.noreply.github.com>
2023-03-09 19:20:33 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,6 +1,6 @@
 def newGit(repo)
 {
-  git "https://github.com/intelliqittrainings/$(repo).git"
+  git "https://github.com/intelliqittrainings/${repo}.git"
 }
 def newMaven()
 {
@@ -8,14 +8,14 @@ def newMaven()
 }
 def newDeploy(jobname,ipaddress,context)
 {
-  sh "scp /var/lib/jenkins/workspace/$(jobname)/webapp/target/webapp.war ubuntu@$(ipaddress):/var/lib/tomcat9/webapps/$(context).war"
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war"
 }
 def runSelenium(jobname)
 {
-  sh "java -jar /var/lib/jenkins/workspace/$(jobname)/testing.jar"
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
 
 def newEmail(body,subject,mailid)
 {
-   mail bcc: '', body: "$(body)", cc: '', from: '', replyTo: '', subject: "$(subject)", to: "$(mailid)"
+  mail bcc: '', body: "${body}", cc: '', from: '', replyTo: '', subject: "${subject}", to: "${mailid}"
 }
