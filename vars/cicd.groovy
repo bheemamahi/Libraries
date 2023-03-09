91aea09263aea62b2af06bd5f3578a3028f25a1f
bheemamahi <126442653+bheemamahi@users.noreply.github.com>
2023-03-09 19:14:56 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,12 @@ def newDeploy(jobname,ipaddress,context)
 {
   sh "scp /var/lib/jenkins/workspace/$(jobname)/webapp/target/webapp.war ubuntu@$(ipaddress):/var/lib/tomcat9/webapps/$(context).war"
 }
+def runSelenium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/$(jobname)/testing.jar"
+}
+
+def newEmail(body,subject,mailid)
+{
+   mail bcc: '', body: "$(body)", cc: '', from: '', replyTo: '', subject: "$(subject)", to: "$(mailid)"
+}
